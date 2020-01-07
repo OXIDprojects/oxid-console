OXIDprojects/oxid-console

commit 21c8ce8dcd13bf79e9ce1b4bbe2a3132c017d699

fix fatal error when loading module commands (#24)
* debug console exit

*  loading commands later

to avoid exceptions in constructor

* code style

* code style

* tracing exit

* trace exit

* reformat code

* do not load commands by composer

this can be done by core console because it is error prone

* trace exit

* debug command scanner

* return after error

* Update CommandCollector.php

* Update CommandCollector.php

* restructure workflow

* fix workflow syntax

* workflow v3 oxid to oxideshop

* simplify error handling

* fix workflow

missing cd /var/www/oxideshop

* Update CommandCollector.php

* Update CommandCollector.php

* Update CommandCollector.php

* avoid loading file in case services.yaml does exist

* fix typo (syntax)

* remove some noise

src/Core/CommandCollector.php
@@ -19,6 +19,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
 use OxidProfessionalServices\OxidConsole\Command\CacheClearCommand;
 use OxidProfessionalServices\OxidConsole\Command\DatabaseUpdateCommand;
 use OxidProfessionalServices\OxidConsole\Command\GenerateMigrationCommand;
@@ -53,10 +54,9 @@ class CommandCollector
         if (!class_exists('oxConsoleCommand')) {
             class_alias(Command::class, 'oxConsoleCommand');
         }
-
         $commands = $this->getCommandsFromCore();
-        $commandsFromModules = $this->getCommandsFromModules();
         $commandsFromComposer = $this->getCommandsFromComposer();
+        $commandsFromModules = $this->getCommandsFromModules();
         return array_merge(
             $commands,
             $commandsFromModules,
@@ -104,6 +104,7 @@ class CommandCollector
             $consoleCommands = isset($oxideshop['console-commands']) && is_array($oxideshop['console-commands']) ?
                 $oxideshop['console-commands'] : [];
             foreach ($consoleCommands as $commandClass) {
+                print "$commandClass is defined in composer.json of module this is depricated\n";
                 $commandsClasses[] = new $commandClass();
             }
             //end of deprecated code
@@ -138,10 +139,9 @@ class CommandCollector
     {
         $oConfig = Registry::getConfig();
 
-
         if (! class_exists(ModuleList::class)) {
             print "ERROR: Oxid ModuleList class can not be loaded,
-             please try to run vendor/bin/oe-eshop-unified_namespace_generator";
+                please try to run vendor/bin/oe-eshop-unified_namespace_generator";
         } else {
             try {
                 $moduleList = oxNew(ModuleList::class);
@@ -197,9 +197,15 @@ class CommandCollector
         $fullModulePaths = array_map(function ($modulePath) use ($modulesRootPath) {
             return $modulesRootPath . $modulePath;
         }, array_values($modulePaths));
-
+        
         return array_filter($fullModulePaths, function ($fullModulePath) {
-            return is_dir($fullModulePath);
+            if (! is_dir($fullModulePath)) {
+                return false;
+            }
+            if (file_exists($fullModulePath . '/services.yaml')) {
+                return false;
+            }
+            return true;
         });
     }
 
@@ -249,12 +255,18 @@ class CommandCollector
      */
     private function getAllClassesFromPhpFile($pathToPhpFile)
     {
+        $name = basename($pathToPhpFile, '.php');
+        echo "depricated command: command $name should be registered in services.yaml";
+
         $classesBefore = get_declared_classes();
         try {
+            echo "scanning $pathToPhpFile...";
             require_once $pathToPhpFile;
+            echo ", file loaded.\n";
         } catch (Throwable $exception) {
             print "Can not add Command $pathToPhpFile:\n";
             print $exception->getMessage() . "\n";
+            return [];
         }
         $classesAfter = get_declared_classes();
         $newClasses = array_diff($classesAfter, $classesBefore);

.github/workflows/check.yml
@@ -22,7 +22,7 @@ jobs:
     needs: stand_alone_checks
     strategy:
       matrix:
-        tag: [v2_6.1_php7.1, v2_6.1_php7.2, v2_6.2-rc_php7.2, v2_6.2-rc_php7.1, v2_dev-master_php7.2]         
+        tag: [v3_6.1_php7.1, v3_6.1_php7.2, v3_6.2-rc_php7.2, v3_6.2-rc_php7.1, v3_dev-master_php7.2]         
     runs-on: ubuntu-latest
     container:
       image: oxidprojects/oxid-test:${{matrix.tag}}
@@ -33,50 +33,43 @@ jobs:
     steps:
     - uses: actions/checkout@v1
     - name: setup oxid
-      run: bash /var/www/oxid/scripts/setupOxid.sh
+      run: bash /var/www/oxideshop/scripts/setupOxid.sh
     - name: install this package into oxid
-      run: bash /var/www/oxid/scripts/setupPackage.sh
-    - name: try to install phpstan-phpunit
-      run: composer require --work-dir=/var/www/oxid --dev phpstan/phpstan-phpunit
-      continue-on-error: true
-    - name: phpstan
-      continue-on-error: true
-      run: |
-        MD=$(pwd)
-        cd /var/www/oxid/
-        touch autoload.oxid.php
-        vendor/bin/phpstan analys --configuration phpstan.neon $MD
+      run: bash /var/www/oxideshop/scripts/setupPackage.sh
     - name: phpstan loose
       continue-on-error: true
       run: |
         MD=$(pwd)
-        cd /var/www/oxid/
+        cd /var/www/oxideshop
         touch autoload.oxid.php
         composer remove phpstan/phpstan-strict-rules --dev
         vendor/bin/phpstan analys  --level 2 --configuration phpstan.neon $MD
     - name: psalm
       run: |
         MD=$(pwd)
-        cd /var/www/oxid
+        cd /var/www/oxideshop
         curl -OL https://circleci.com/api/v1.1/project/github/weirdan/psalm/232/artifacts/0/home/docker/project/build/psalm.phar
         php psalm.phar --stats --show-info=true $MD
-    - name: run
-      continue-on-error: true
+    - name: run console 
       run: |
-        cd /var/www/oxid/
+        cd /var/www/oxideshop
         vendor/bin/oxid list
-    - name: run module internals
+    - name: install module internals
       run: |
-        cd /var/www/oxid
+        cd /var/www/oxideshop
         composer require "oxid-community/moduleinternals:dev-master"
         vendor/bin/oxid list
+    - name: run module internals
+      run : |
+        cd /var/www/oxideshop
         vendor/bin/oxid module:activate moduleinternals
+        vendor/bin/oxid list
         vendor/bin/oxid fix:states -a
         ls -al source/log
     - name: done
       if: failure()
       run: |
-        cd /var/www/oxid
+        cd /var/www/oxideshop
         ls -al source/log
         cat source/log/*
         

bin/oxid
@@ -3,38 +3,31 @@
 
 use OxidProfessionalServices\OxidConsole\Core\Application;
 
-function outputError($e) {
-    print "Oxid Professional Services Console is sorry about: \n";
-    $trace = $e->getTrace();
-    $cause = $trace[0];
 
-    $result = 'Exception: "';
-    $result .= $e->getMessage();
-    $result .= '" @ ';
-    $result .= $cause['file'].':'.$cause['line']."\n";
-    if($cause['class'] != '') {
-      $result .= $cause['class'];
-      $result .= '->';
-    }
-    $result .= $cause['function'];
-    $result .= "();\n";
-
-    //console should print errors always
-    print $e->getTraceAsString() . "\n";
-    //shop exception handler may take care about error code and logging
-}
-    
 function shutdown() {
-    $e = error_get_last();
-    if ($e) {      
-        print "Oxid Professional Services Console is sorry to be interrupted by an error:";
-        print_r($e);
+    $error = error_get_last();
+    if ($error['type'] === E_ERROR) {
+        // fatal error has occured
+        $trace = array_reverse($GLOBALS['dbg_stack']);
+        array_pop($trace);
+        
+        echo 'Backtrace for: \'' . $error['message'] . '\' at ' . $error['file'] . ':' . $error['line'] . ':' . "\n";
+        foreach($trace as $item) {
+           echo '  ' . (isset($item['file']) ? $item['file'] : '<unknown file>')
+           . ':' . (isset($item['line']) ? $item['line'] : '<unknown line>')
+           . ' calling ' . $item['function'] . '()' . "\n";
+        }    
     }
-    print "Oxid Professional Services Console is done\n";
 }
-
-register_shutdown_function("shutdown");
-
+register_shutdown_function('shutdown');
+function write_dbg_stack() {
+    $GLOBALS['dbg_stack'] = debug_backtrace();
+}
+register_tick_function('write_dbg_stack');
+declare(ticks=1);
+//using the console is for developers
+//all errors should be shown to them
+ini_set('display_error',1);
 print "Oxid Professional Services consle  is called by $argv[0]\n";
 $binFolder = dirname($argv[0]);
 $autoloadFile = $binFolder.'/../autoload.php';
@@ -42,10 +35,5 @@ $autoloadFile = $binFolder.'/../autoload.php';
 require_once $autoloadFile;
 $projectRoot = realpath(dirname(dirname($binFolder)));
 
-try {
-    $application = new Application($projectRoot);
-    $application->run();
-} catch (Throwable $e) {
-    outputError($e);
-    throw $e;
-}
+$application = new Application($projectRoot);
+$application->run();

src/Core/Application.php
@@ -34,7 +34,11 @@ class Application extends BaseApplication
         $v = new VersionHelper($projectRoot);
         $version = $v->getVersion('oxid-professional-services/oxid-console');
         parent::__construct($name, $version);
+    }
 
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        $projectRoot = $this->projectRoot;
         print "Oxid project root is found at $projectRoot\n";
         chdir($projectRoot);
 
@@ -72,6 +76,7 @@ class Application extends BaseApplication
             }
         }
         echo "commands added\n";
+        parent::doRun($input, $output);
     }
 
     public function loadBootstrap()

commit 4c47dde9240c046e3e47512932c66d85fa9f104d

fixed typo introduced in #24

src/Core/CommandCollector.php
@@ -104,7 +104,7 @@ class CommandCollector
             $consoleCommands = isset($oxideshop['console-commands']) && is_array($oxideshop['console-commands']) ?
                 $oxideshop['console-commands'] : [];
             foreach ($consoleCommands as $commandClass) {
-                print "$commandClass is defined in composer.json of module this is depricated\n";
+                print "$commandClass is defined in composer.json of module this is deprecated\n";
                 $commandsClasses[] = new $commandClass();
             }
             //end of deprecated code
@@ -197,7 +197,7 @@ class CommandCollector
         $fullModulePaths = array_map(function ($modulePath) use ($modulesRootPath) {
             return $modulesRootPath . $modulePath;
         }, array_values($modulePaths));
-        
+
         return array_filter($fullModulePaths, function ($fullModulePath) {
             if (! is_dir($fullModulePath)) {
                 return false;